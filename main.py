bfc3d1d1cf6d610829709bc3f81d315298cd767d
Romain CLEMENT <rc2301@orange.fr>
2023-06-11 17:48:13 +0200

    Added top level window

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 
-if __name__ == "__main__":
-    window = tk.Tk()
-    tk.Label(window, text="Hello World!").pack()
-    window.mainloop()
+window = tk.Tk()
+
+def settings():
+    top_level = tk.Toplevel(window)
+    top_level.title("Top level")
+    tk.Label(top_level, text="elo").pack()
+
+tk.Label(window, text="Hello World!").pack()
+tk.Button(window, text="Settings", command=settings).pack()
+window.mainloop()
